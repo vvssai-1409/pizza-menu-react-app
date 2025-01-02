9b59c2f61626b8be60aa5604b27af6a054459e97
vvssai-1409 <vvs.sai1409@gmail.com>
2025-01-02 16:11:55 +0530

    Updated index.js
    Rendering the list

--- src/index.js
+++ src/index.js
@@ -60,11 +60,11 @@ function App() {
 function Pizza(props) {
   return (
     <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
     </div>
   );
@@ -88,7 +88,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>;
-      <Pizza
+      <div>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} />
+        ))}
+      </div>
+      {/* <Pizza
         name="Focaccia"
         ingredients="Bread with italian olive oil and rosemary"
         price={6}
@@ -123,7 +128,7 @@ function Menu() {
         ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
         price={18}
         photoName="pizzas/prosciutto.jpg"
-      />
+      /> */}
     </main>
   );
 }
